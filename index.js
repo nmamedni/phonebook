727a2f3bcb5b62b592a6bee69cdfde698a92ab3e
Natali Mamedniyazova <natali.mamedniyazova@gmx.de>
2026-01-28 11:20:48 +0100

    all requests via MongoDb

--- index.js
+++ index.js
@@ -16,13 +16,12 @@ const app = express()
 morgan.token('content', function (req, res) { return JSON.stringify(req.body) })
 morgan.token('date', function (req, res) { return new Date().toString() })
 
+app.use(express.static('dist'))
+app.use(express.json())
 // var corsOptions = {
 //   origin: 'http://localhost:5174',
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
-
-app.use(express.json())
-app.use(express.static('dist'))
 // app.use(cors(corsOptions))
 // app.use(morgan('tiny'))
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content :date'))
@@ -58,10 +57,9 @@ app.get(`/api/persons`, (request, response) => {
     console.log("people=", people)
     response.status(200).json(people)  
   })
-  // response.status(200).json(phonebook)
 })
 
-app.get(`/api/info`, (request, response) => {
+app.get(`/info`, (request, response) => {
   console.log("--- get Info request headers:", request.headers)
   Person.countDocuments({})
   .then(count => {
@@ -74,28 +72,48 @@ app.get(`/api/info`, (request, response) => {
   })
 })
 
-app.get(`/api/persons/:id`, (request, response) => {
+app.get(`/api/persons/:id`, (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    console.log(`---Found entry ${person?.id}`)
-    response.status(200).json(person)
-  })
-  // if (entry === undefined) {
-  //   // response.statusMessage = `Entry ${request.params.id} doesn't exist on the server`;
-  //   return response.status(404).end()
-  // }
+    .then(person => {
+      if (person) {
+        console.log(`---Found entry ${person?.id}`)
+        response.status(200).json(person)
+      }
+      else {
+        // response.statusMessage = `Entry ${request.params.id} doesn't exist on the server`;
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete(`/api/persons/:id`, (request, response) => {
-  const id = request.params.id
-  phonebook = phonebook.filter(item => item?.id !== id)
-  // if (entry === undefined) {
-  //   return response.status(404).end()
-  // }
-  response.status(204).end()
+app.delete(`/api/persons/:id`, (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post(`/api/persons`, (request, response) => {
+app.put(`/api/persons/:id`, (request, response, next) => {
+  const name = request?.body?.name
+  const number = request?.body?.number
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+      person.name = name
+      person.number = number
+      return person.save()
+            .then(updatedPerson => {
+              response.json(updatedPerson)
+            })
+    })
+    .catch(error => next(error))
+})
+
+app.post(`/api/persons`, (request, response, next) => {
   console.log("---Post request headers: host = ", request.get("host"))
   console.log("---Post request headers: origin = ", request.get("origin"))
   console.log("---Post request headers: referer = ", request.get("referer"))
@@ -119,7 +137,27 @@ app.post(`/api/persons`, (request, response) => {
   .then(savedPerson => {
     response.status(200).json(savedPerson)
   })
+  .catch(error => next(error))
 })
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({error: "malformatted id"})
+  }
+
+  next(error)
+}
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
